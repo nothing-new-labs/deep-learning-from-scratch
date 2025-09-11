2d722bf135c63f1f6800852b0480e3dbed393a26
DavidLi <davidli2010@foxmail.com>
2025-09-11 23:05:10 +0800

    chapter 4

--- common/sigmoid.py
+++ common/sigmoid.py
@@ -3,6 +3,9 @@ import numpy as np
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
+def sigmoid_grad(x):
+    return (1.0 - sigmoid(x)) * sigmoid(x)
+
 class Sigmoid:
     def __init__(self):
         self.out = None
